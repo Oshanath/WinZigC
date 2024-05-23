055e2802568a868c66e6916cfa3a9b97194675ea
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2024-05-23 19:09:15 +0530

    Lexer started and test programs added

---
+++ src/WinZigProgramReader.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WinZigProgramReader {
+    public String readProgram(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        StringBuilder stringBuilder = new StringBuilder();
+        String ls = System.getProperty("line.separator");
+
+        String line;
+        while((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+        reader.close();
+        return stringBuilder.toString() + "   ";
+    }
+}
