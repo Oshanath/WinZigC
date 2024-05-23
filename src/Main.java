7e3bd991abad4631cfcedf2dfaa7f1d2e0d9b416
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2024-05-23 22:38:25 +0530

    lexer testing done

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,16 @@ public class Main {
         Screener screener = new Screener();
         String program_string = null;
         try {
-            program_string = winZigProgramReader.readProgram("E:\\Aca\\8th Semester\\CS4542 - Compiler Design\\Project\\WinZigC\\winzig_test_programs\\winzig_01");
+            program_string = winZigProgramReader.readProgram("E:\\Aca\\8th Semester\\CS4542 - Compiler Design\\Project\\WinZigC\\winzig_test_programs\\winzig_02");
             Lexer lexer = new Lexer(program_string);
             List<Token> scannedTokenList = lexer.getTokenList();
             List<Token> screenedTokenList = screener.getScreenedTokenList(scannedTokenList);
+
+            for (Token token : screenedTokenList) {
+                System.out.println(token.type + ":" + token.text);
+            }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println(program_string);
-
-        //Testing
-//        char[] white_space_chars = {'\t', '\r', '\f', ' '};
-//        Lexer lexer = new Lexer("#f5g67");
-//        System.out.println(lexer.getIdentifier());
     }
 }
--- src/WinZigProgramReader.java
+++ src/WinZigProgramReader.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 public class WinZigProgramReader {
     public String readProgram(String path) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(path));
-        StringBuilder stringBuilder = new StringBuilder();
-        String ls = System.getProperty("line_number.separator");
-
         String line;
-        while((line = reader.readLine()) != null) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String ls = System.getProperty("line.separator");
+        while ((line = reader.readLine()) != null) {
             stringBuilder.append(line);
             stringBuilder.append(ls);
         }
-        reader.close();
-        return stringBuilder.toString() + "   ";
+        return stringBuilder + "   ";
     }
 }
