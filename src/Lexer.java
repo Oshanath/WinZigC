91894c6ac03e1332b653af2ec133589a07dd9853
Jithmi004 <79504341+Jithmi004@users.noreply.github.com>
2024-05-23 22:18:09 +0530

    Lexer finished and screener added

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Lexer {
@@ -17,7 +19,7 @@ public class Lexer {
         return this.program_string.charAt(position);
     }
 
-    public Token getNewLine(){
+    private Token getNewLine(){
         int start_position = this.position;
         if(getCurrentChar() == '\n'){
             this.position++;
@@ -28,7 +30,7 @@ public class Lexer {
         return null;
     }
 
-    public Token getWhiteSpace(){
+    private Token getWhiteSpace(){
         char[] white_space_chars = {'\t', '\r', '\f', ' '};
         int start_position = this.position;
         if (isMember(getCurrentChar(), white_space_chars)){
@@ -41,7 +43,7 @@ public class Lexer {
         return null;
     }
 
-    public boolean isMember(char value, char[] charList){
+    private boolean isMember(char value, char[] charList){
         for (char ch : charList){
             if(ch == value){
                 return true;
@@ -50,7 +52,7 @@ public class Lexer {
         return false;
     }
 
-    public String getIdentifier(){
+    private String getIdentifier(){
         String alphabeticalAndSpecialChar = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String digits = "0123456789";
         if (alphabeticalAndSpecialChar.indexOf(getCurrentChar()) >= 0){
@@ -64,7 +66,7 @@ public class Lexer {
         return null;
     }
 
-    public Token getInteger(){
+    private Token getInteger(){
         String digits = "0123456789";
         if (digits.indexOf(getCurrentChar()) >= 0){
             int start_position = this.position;
@@ -77,7 +79,7 @@ public class Lexer {
         return null;
     }
 
-    public Token getChar(){
+    private Token getChar(){
         int start_position = this.position;
         if (getCurrentChar() == '\'' & this.program_string.charAt(this.position + 2) == '\'' & this.program_string.charAt(this.position + 1) != '\'') {
             this.position += 3;
@@ -86,7 +88,7 @@ public class Lexer {
         return null;
     }
 
-    public Token getString(){
+    private Token getString(){
         int start_position = this.position;
         if (getCurrentChar() == '"') {
             do {
@@ -100,7 +102,7 @@ public class Lexer {
         return null;
     }
 
-    public Token getCommentType1(){
+    private Token getCommentType1(){
         int start_position = this.position;
         if (getCurrentChar() == '#') {
             do {
@@ -114,7 +116,7 @@ public class Lexer {
         return null;
     }
 
-    public Token getCommentType2(){
+    private Token getCommentType2(){
         int start_position = this.position;
         Stack<Character> nestedComments = new Stack<>();
         if (getCurrentChar() == '{') {
@@ -146,8 +148,182 @@ public class Lexer {
         return null;
     }
 
-    public Token getNextToken(){
-        //TODO
-        return null;
+    private Token getNextToken(){
+        if(this.position >= this.program_string.length()){
+            return new Token(Scanner.TOKEN.END_OF_PROGRAM,"\0",this.position,this.line_number,this.column_number);
+        }
+        String token = getIdentifier();
+        if (token != null) {
+            int start_position = this.position;
+            switch (token){
+                case "program":
+                    return new Token(Scanner.TOKEN.PROGRAM,token,start_position,this.line_number,this.column_number);
+                case "var":
+                    return new Token(Scanner.TOKEN.VAR,token,start_position,this.line_number,this.column_number);
+                case "const":
+                    return new Token(Scanner.TOKEN.CONST,token,start_position,this.line_number,this.column_number);
+                case "type":
+                    return new Token(Scanner.TOKEN.TYPE,token,start_position,this.line_number,this.column_number);
+                case "function":
+                    return new Token(Scanner.TOKEN.FUNCTION,token,start_position,this.line_number,this.column_number);
+                case "return":
+                    return new Token(Scanner.TOKEN.RETURN,token,start_position,this.line_number,this.column_number);
+                case "begin":
+                    return new Token(Scanner.TOKEN.BEGIN,token,start_position,this.line_number,this.column_number);
+                case "end":
+                    return new Token(Scanner.TOKEN.END,token,start_position,this.line_number,this.column_number);
+                case "output":
+                    return new Token(Scanner.TOKEN.OUTPUT,token,start_position,this.line_number,this.column_number);
+                case "if":
+                    return new Token(Scanner.TOKEN.IF,token,start_position,this.line_number,this.column_number);
+                case "then":
+                    return new Token(Scanner.TOKEN.THEN,token,start_position,this.line_number,this.column_number);
+                case "else":
+                    return new Token(Scanner.TOKEN.ELSE,token,start_position,this.line_number,this.column_number);
+                case "while":
+                    return new Token(Scanner.TOKEN.WHILE,token,start_position,this.line_number,this.column_number);
+                case "do":
+                    return new Token(Scanner.TOKEN.DO,token,start_position,this.line_number,this.column_number);
+                case "case":
+                    return new Token(Scanner.TOKEN.CASE,token,start_position,this.line_number,this.column_number);
+                case "of":
+                    return new Token(Scanner.TOKEN.OF,token,start_position,this.line_number,this.column_number);
+                case "otherwise":
+                    return new Token(Scanner.TOKEN.OTHERWISE,token,start_position,this.line_number,this.column_number);
+                case "repeat":
+                    return new Token(Scanner.TOKEN.REPEAT,token,start_position,this.line_number,this.column_number);
+                case "for":
+                    return new Token(Scanner.TOKEN.FOR,token,start_position,this.line_number,this.column_number);
+                case "until":
+                    return new Token(Scanner.TOKEN.UNTIL,token,start_position,this.line_number,this.column_number);
+                case "loop":
+                    return new Token(Scanner.TOKEN.LOOP,token,start_position,this.line_number,this.column_number);
+                case "pool":
+                    return new Token(Scanner.TOKEN.POOL,token,start_position,this.line_number,this.column_number);
+                case "exit":
+                    return new Token(Scanner.TOKEN.EXIT,token,start_position,this.line_number,this.column_number);
+                case "mod":
+                    return new Token(Scanner.TOKEN.MOD,token,start_position,this.line_number,this.column_number);
+                case "and":
+                    return new Token(Scanner.TOKEN.AND,token,start_position,this.line_number,this.column_number);
+                case "or":
+                    return new Token(Scanner.TOKEN.OR,token,start_position,this.line_number,this.column_number);
+                case "not":
+                    return new Token(Scanner.TOKEN.NOT,token,start_position,this.line_number,this.column_number);
+                case "read":
+                    return new Token(Scanner.TOKEN.READ,token,start_position,this.line_number,this.column_number);
+                case "succ":
+                    return new Token(Scanner.TOKEN.SUCC,token,start_position,this.line_number,this.column_number);
+                case "pred":
+                    return new Token(Scanner.TOKEN.PRED,token,start_position,this.line_number,this.column_number);
+                case "chr":
+                    return new Token(Scanner.TOKEN.CHR,token,start_position,this.line_number,this.column_number);
+                case "ord":
+                    return new Token(Scanner.TOKEN.ORD,token,start_position,this.line_number,this.column_number);
+                case "eof":
+                    return new Token(Scanner.TOKEN.EOF,token,start_position,this.line_number,this.column_number);
+                default:
+                    return new Token(Scanner.TOKEN.IDENTIFIER,token,start_position,this.line_number,this.column_number - token.length());
+            }
+        }
+
+        //new line
+        Token newLineToken = getNewLine();
+        if(newLineToken != null){
+            return newLineToken;
+        }
+        //white space
+        Token whiteSpaceToken = getWhiteSpace();
+        if(whiteSpaceToken != null){
+            return whiteSpaceToken;
+        }
+        //integer
+        Token integerToken = getInteger();
+        if(integerToken != null){
+            return integerToken;
+        }
+        //char
+        Token charToken = getChar();
+        if(charToken != null){
+            return charToken;
+        }
+        //string
+        Token stringToken = getString();
+        if(stringToken != null){
+            return stringToken;
+        }
+        //comment type 1
+        Token commentType1Token = getCommentType1();
+        if(commentType1Token != null){
+            return commentType1Token;
+        }
+        //comment type 2
+        Token commentType2Token = getCommentType2();
+        if(commentType2Token != null){
+            return commentType2Token;
+        }
+        //swap
+        String swapString = this.program_string.substring(this.position, this.position + 3);
+        if(swapString.equals(":=:")){
+            return new Token(Scanner.TOKEN.SWAP,swapString,this.position+=3,this.line_number,this.column_number);
+        }
+        //tokens of length 2
+        String tokenOfLen2 = this.program_string.substring(this.position, this.position + 2);
+        switch(tokenOfLen2){
+            case ":=" :
+                return new Token(Scanner.TOKEN.ASSIGN, tokenOfLen2,this.position+=2, this.line_number,this.column_number);
+            case ".." :
+                return new Token(Scanner.TOKEN.DDOT, tokenOfLen2,this.position+=2, this.line_number,this.column_number);
+            case "<=" :
+                return new Token(Scanner.TOKEN.LEQ, tokenOfLen2,this.position+=2, this.line_number,this.column_number);
+            case "<>" :
+                return new Token(Scanner.TOKEN.NEQ, tokenOfLen2,this.position+=2, this.line_number,this.column_number);
+            case ">=" :
+                return new Token(Scanner.TOKEN.GEQ, tokenOfLen2,this.position+=2, this.line_number,this.column_number);
+        }
+        //tokens of length 1
+        char tokenOfLen1 = getCurrentChar();
+        switch(tokenOfLen1){
+            case '<' :
+                return new Token(Scanner.TOKEN.LT, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '>' :
+                return new Token(Scanner.TOKEN.GT, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '=' :
+                return new Token(Scanner.TOKEN.EQ, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case ':' :
+                return new Token(Scanner.TOKEN.COLON, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case ';' :
+                return new Token(Scanner.TOKEN.SEMI_COLON, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '.' :
+                return new Token(Scanner.TOKEN.DOT, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case ',' :
+                return new Token(Scanner.TOKEN.COMMA, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '(' :
+                return new Token(Scanner.TOKEN.BRACKET_BEGIN, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case ')' :
+                return new Token(Scanner.TOKEN.BRACKET_END, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '+' :
+                return new Token(Scanner.TOKEN.PLUS, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '-' :
+                return new Token(Scanner.TOKEN.MINUS, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '*' :
+                return new Token(Scanner.TOKEN.MULT, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+            case '/' :
+                return new Token(Scanner.TOKEN.DIV, String.valueOf(tokenOfLen1),++this.position, this.line_number,this.column_number);
+        }
+        return new Token(Scanner.TOKEN.ERROR,String.valueOf(tokenOfLen1),this.position, this.line_number,this.column_number);
+    }
+
+    public List<Token> getTokenList(){
+        List<Token> tokenList = new ArrayList<>();
+        Token token;
+        do {
+            token = this.getNextToken();
+            tokenList.add(token);
+            if (token.type.equals(Scanner.TOKEN.ERROR)) {
+                break;
+            }
+        } while (!token.type.equals(Scanner.TOKEN.END_OF_PROGRAM));
+        return tokenList;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         WinZigProgramReader winZigProgramReader = new WinZigProgramReader();
-
+        Screener screener = new Screener();
         String program_string = null;
         try {
             program_string = winZigProgramReader.readProgram("E:\\Aca\\8th Semester\\CS4542 - Compiler Design\\Project\\WinZigC\\winzig_test_programs\\winzig_01");
+            Lexer lexer = new Lexer(program_string);
+            List<Token> scannedTokenList = lexer.getTokenList();
+            List<Token> screenedTokenList = screener.getScreenedTokenList(scannedTokenList);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
